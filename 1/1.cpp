b90bed56f46c252679ce7e8d178aa6e8d46b96f2
PPODO <sherlockwhooo@gmail.com>
2019-12-13 01:07:08 +0900

    1

---
+++ 1/1.cpp
@@ -0,0 +1,18 @@
+﻿#include "1.h"
+
+int main() {
+#ifdef _WIN32
+	WSADATA WinSockData;
+	if (WSAStartup(WINSOCK_VERSION, &WinSockData) == SOCKET_ERROR) {
+		std::cout << "Failed To Initialize WinSock Application! : " << WSAGetLastError() << std::endl;
+	}
+#endif
+
+
+#ifdef _WIN32
+	if (WSACleanup() == SOCKET_ERROR) {
+		std::cout << "Failed To Cleanup WinSock Application! : " << WSAGetLastError() << std::endl;
+	}
+#endif
+	return 0;
+}
---
+++ 1/1.h
@@ -0,0 +1,11 @@
+﻿#pragma once
+
+#include <iostream>
+#ifdef _WIN32
+#pragma comment(lib, "ws2_32.lib");
+#define _WIN32_LEAN_AND_MEAN
+#include <WinSock2.h>
+#else if
+#include <sys/socket.h> // for socket
+#include <netinet/ip.h>
+#endif
