fd2529a87fe0637def7c436b7aadd3d2afa2a45e
PPODO <sherlockwhooo@gmail.com>
2019-12-15 20:07:35 +0900

    Linux Study

--- Linux_file1/main.cpp
+++ Linux_file1/main.cpp
@@ -6,13 +6,21 @@
 
 int main() {
     int FileID = 0;
-    if((FileID = open("test.txt", O_WRONLY | O_CREAT | O_EXCL)) == -1) {
-        if(errno == EEXIST) {
-            std::cout << "File Exist!\n";
-            close(FileID);
+    if((FileID = open("test.txt", O_RDONLY)) == -1) {
+        if(errno == ENOEXEC) {
+            std::cout << "File Doesn't Exist!\n";
             return -1;
         }
     }
 
+    char ReadBuffer[1024] = { "\0" };
+    if(read(FileID, ReadBuffer, 1024) == -1) {
+        close(FileID);
+        return -1;
+    }
+
+    std::cout << ReadBuffer << std::endl;
+
+    close(FileID);
     return 0;
 }
