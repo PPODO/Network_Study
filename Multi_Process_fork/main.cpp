6ed5645d42f87f78973d086d5c6418979c75a9cb
PPODO <sherlockwhooo@gmail.com>
2019-12-27 04:45:12 +0900

    Multi Process Programming by fork

---
+++ Multi_Process_fork/main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <unistd.h>
+
+int main() {
+    std::cout << "Program Start!!\n";
+
+    pid_t PID = fork();
+    if(PID < 0) {
+        std::cout << "Fork Failure!\n";
+        return -1;
+    }
+    std::cout << "Fork!!!\n";
+
+    if(PID == 0) {
+        std::cout << "I'm Child Process : " << getpid() << std::endl;
+
+        int i = 0;
+        while(true) {
+            std::cout << "Child : " << i << std::endl;
+            i += 1;
+            sleep(1);
+        }
+    }
+    else if(PID > 0) {
+        std::cout << "I'm Parent Process : " << getpid() << std::endl;
+
+        int i = 0;
+        while(true) {
+            std::cout << "Parent : " << i << std::endl;
+            i += 2;
+            sleep(1);
+        }
+    }
+    return 0;
+}
